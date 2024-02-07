142935108c477e1059afbd9448588f2528c773da
Appetizing <kvo0731@gmail.com>
2024-02-06 23:34:55 -0700

    Created Variables
    Created variables to link with the buttons

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+
+
+const numButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('.equal');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const decimalButton = document.querySelector('.decimal');
+const lastOperation = document.querySelector('.previousOperation');
+const currentOperation = document.querySelector('.currentOperationScreen')
+
+window.addEventListener('keydown', handleKeyboardInput);
+equalButton.addEventListener('click', )
+clearButton.addEventListener('click', )
+deleteButton.addEventListener('click', )
+decimalButton.addEventListener('click', )
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -15,5 +32,9 @@ function divide(num1, num2){
 }
 
 function operate(num1, operator, num2){
-    
+
+}
+
+function display(num){
+
 }
